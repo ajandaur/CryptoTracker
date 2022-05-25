b0c47805e7bd36687fd14a9f63142dab337c1175
ajandaur <anmol.jandaur@gmail.com>
2022-05-25 13:57:18 -0700

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Anmol  Jandaur on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
